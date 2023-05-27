117782677a832cf479f6e7bf5b3891ee4d457fa5
CWillwohl <caio.w.ribeiro@gmail.com>
2023-05-27 09:10:17 -0300

    Laravel Breeze Authentication Installed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-guest-layout>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-
-        <style>
-            .h-screen {
-                min-height: 100vh;
-            }
-        </style>
-
-    </head>
-    <body>
-        <div class="container-fluid h-screen d-flex align-items-center justify-content-center">
-            <h1>Hello World!</h1>
+    <div class="w-full min-h-screen flex justify-center items-center">
+        <div class="gap-4">
+            <a href="{{ route('login') }}" class="bg-zinc-800 hover:bg-zinc-900  text-gray-50 p-4 uppercase font-semibold">Sign In</a>
+            <a href="{{ route('register') }}" class="bg-zinc-800 text-gray-50 p-4 uppercase font-semibold">Sign Up</a>
         </div>
-    </body>
-</html>
+    </div>
+
+</x-guest-layout>
+
